5103c3e61132c97fa4971d9c173cc2d2992348be
der DunkelHeit <almeida.junior.nelson@escola.pr.gov.br>
2026-09-02 15:28:46 +0000

    codigo puro.

--- priN.c
+++ priN.c
@@ -1,1 +1,16 @@
-//Esse programa cria um arquivo no formato texto pela classe do primeiro ano AMS.
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *arquivo = fopen("arquivo.txt", "w");
+
+	if (arquivo == NULL)
+	{
+		perror("Nao foi possivel abrir o arquivo");
+		return 1;
+	}
+
+	fclose(arquivo);
+	return 0;
+}
+//https://github.com/lehrerNelson/primeiroN.git
