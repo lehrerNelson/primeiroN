f82fe90810d5e395a6174f236eca6d06859631d5
der DunkelHeit <almeida.junior.nelson@escola.pr.gov.br>
2026-09-02 15:31:16 +0000

    dar nome ao arquivo.

--- priN.c
+++ priN.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	FILE *arquivo = fopen("arquivo.txt", "w");
+	char nomeArquivo[100];
+
+	printf("Digite o nome do arquivo: ");
+	if (fgets(nomeArquivo, sizeof(nomeArquivo), stdin) == NULL)
+	{
+		return 1;
+	}
+
+	nomeArquivo[strcspn(nomeArquivo, "\n")] = '\0';
+	FILE *arquivo = fopen(nomeArquivo, "w");
 
 	if (arquivo == NULL)
 	{
